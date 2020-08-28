9bf4b4b44b3594089e94f0547ba0bcaf5c9c534d
shabidkhan <shabid19@navgurukul.org>
2020-08-28 20:07:25 +0530

    Edit commit

--- webtask14.py
+++ webtask14.py
@@ -1,41 +1,34 @@
-from webtask13 import *
-def movies_(i,c):
-	c+=1
-	dic={}
-	id_dic={}
-	dic['name']=i[0]['name']
-	dic1={}
-	list1=[]
-	dic1['imdb_id']=str(i[1]['imdb_id'])
-	dic1['name']=str(i[1]['name'])
-	count=0
-	for j in movies_actor:
-		if c<len(j):
-			if str(i[1]['name'])==str(j[c]['name']):
-				count+=1
-	dic1['num_mov']=count
-	list1.append(dic1)
-	dic["frequent_co_actors"]=str(list1)
-	new_dict=(dic)
-	id_dic[(i[0]['imdb_id'])]=new_dict
-	if len(i)==2:
-		final_list.append(str(id_dic))
-		return final_list
-	final_list.append(str(id_dic))
-	final_list.append(movies_(i[1:],c))
-	return final_list
+import json,pprint
+with open("webtask12.json",'r') as file:
+	movie_actors_list = json.load(file)
 
 
 def function():
+	MoviesCastList = []
+	for CastList in movie_actors_list:
+		CastDict={}
+		for CastIndex in range(len(CastList)-1):
+			CoActorsList=[]
+			CoActorsDict={}
+			CoActorsDict['imdb_id'] = CastList[CastIndex+1]["imdb_id"]
+			CoActorsDict["name"] = CastList[CastIndex+1]['name']
+			Count = 0
+			for Movies in movie_actors_list :
+				if CastList[CastIndex] in Movies and CastList[CastIndex+1] in Movies:
+					Count+=1
+			CoActorsDict['num_movies'] = Count
+			CoActorsList.append(CoActorsDict)
+			CastDict[CastList[CastIndex]['imdb_id']]={
+				"Name":CastList[CastIndex]['name'],
+				'frequent_co_actors':CoActorsList
+			}
 
-	c=0
-	co_actors=[]
-	for n in movies_actor:
-		
-		l=movies_(n,c)
-		co_actors.append((l[:len(n)-1]))
-	return(co_actors)
+		MoviesCastList.append(CastDict)
+	return MoviesCastList	
 
 final_list=[]
 if __name__ == '__main__':
 	pprint.pprint(function())
+
+
+	
